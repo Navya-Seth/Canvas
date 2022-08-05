6c1162afabe986438a04c3b94743ec9ccf828a93
Navya-Seth <91605093+Navya-Seth@users.noreply.github.com>
2022-08-05 13:41:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,41 @@
 canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext("2d");
-Color = "yellow";
-ctx.beginPath();
-ctx.strokeStyle = Color;
-ctx.lineWidth = 4;
-ctx.arc(200,200,40,0,2*Math.PI);
-ctx.stroke();
-canvas.addEventListener("mousedown",mymousedowm);
-
-function mymousedowm(e){
-    Color = document.getElementById("color").value;
-    console.log(Color);
-    mouse_x = e.clientX - canvas.offsetLeft ;
-    mouse_y = e.clientY - canvas.offsetTop ;
-    console.log("x =" + mouse_x + "y =" + mouse_y);
-    circle(mouse_x , mouse_y)
+var mouseEvent = "empty";
+var last_position_of_x, last_position_of_y;
+color = "yellow";
+width_of_line = 1;
+canvas.addEventListener("mousedown" , my_mousedown);
+function my_mousedown(e){
+    mouseEvent = "mousedown";
 }
 
-function circle (mouse_x , mouse_y) {
+canvas.addEventListener("mouseup" , my_mouseup);
+function my_mouseup(e){
+    mouseEvent = "mouseup";
+}
+
+canvas.addEventListener("mouseleave" , my_mouseleave);
+function my_mouseleave(e){
+    mouseEvent = "mouseleave";
+}
+
+canvas.addEventListener("mousemove" , my_mousemove);
+function my_mousemove(e){
+   current_position_of_x = e.clientX - canvas.offsetLeft;
+   current_position_of_y = e.clientY - canvas.offsetTop;
+   if (mouseEvent == "mousedown"){
     ctx.beginPath();
-    ctx.strokeStyle = Color;
-ctx.lineWidth = 4;
-ctx.arc(mouse_x,mouse_y,40,0,2*Math.PI);
-ctx.stroke();
-}
-    
-function cleararea();{
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width_of_line;
+    console.log("last position of x and y = ");
+    console.log("x = "+ last_position_of_x + "y = "+last_position_of_y);
+    ctx.moveTo(last_position_of_x,last_position_of_y);
+
+    console.log("current position of x and y = ");
+    console.log("x = "+ current_position_of_x + "y = "+current_position_of_y);
+    ctx.lineTo(current_position_of_x,current_position_of_y);
+    ctx.stroke();
+   }
+   last_position_of_x = current_position_of_x;
+   last_position_of_y = current_position_of_y;
 }
